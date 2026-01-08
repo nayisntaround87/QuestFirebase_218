41c152e3bce37e3584e194dc94119f5909cf469f
Nayla Salwa <naylasalwahayati@gmail.com>
2026-01-08 09:07:11 +0700

    menambahkan DestinasiEntry.kt

---
+++ app/src/main/java/com/example/firebase/view/Route/DestinasiEntry.kt
@@ -0,0 +1,7 @@
+package com.example.firebase.view.Route
+
+import com.example.firebase.R
+
+object DestinasiEntry : DestinasiNavigasi{
+    override val route = "item_entry"
+    override  val titleRes = R.string.entry_siswa}
