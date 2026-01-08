16ed92a72728d7914184dc165ccbfccf8059704b
Nayla Salwa <naylasalwahayati@gmail.com>
2026-01-08 09:08:30 +0700

    menambahkan DestinasiHome.kt

---
+++ app/src/main/java/com/example/firebase/view/Route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.firebase.view.Route
+
+import com.example.firebase.R
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
